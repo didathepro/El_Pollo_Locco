173c2140ffde9a4d626f0511800652dcbfbcc1db
didathepro <didovicgraz@gmail.com>
2024-11-02 16:18:04 +0100

    Frames

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -31,6 +31,18 @@ class MoveableObject {
         })
     }
 
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.height, this.width);
+    }
+
+    showFrame(ctx){
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle= 'blue';
+        ctx.rect(this.x, this.y, this.height, this.width);
+        ctx.stroke();
+    }
+
     playAnimation(images){
         let i = this.currentImage % this.IMAGES_WALKING.length; 
         let path = images[i];
