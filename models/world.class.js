58b69d685722c0046e0122acf16e25ab6a3b5a2b
didathepro <didovicgraz@gmail.com>
2024-11-02 18:09:05 +0100

    Update world.class.js

--- models/world.class.js
+++ models/world.class.js
@@ -25,7 +25,7 @@ class World {
             this.level.enemies.forEach((enemy)=>{
                 if(this.character.isColliding(enemy)){
                     this.character.hit();
-                    console.log(this.character.energy)
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             })
         },200);
@@ -35,9 +35,12 @@ class World {
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 
         this.ctx.translate(this.camera_x,0);
-
         this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x,0);
         this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x,0);
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
